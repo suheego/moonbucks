20ae9ff58fc4c43eec636d79b4a106c071d4c230
고수희 <dev.suhee@gmail.com>
2022-07-02 18:00:44 +0900

    refactor : submit

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,17 @@
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
-  // Form 태그가 자동으로 전송되는 것을 막아줌
-  $('#espresso-menu-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-  });
-
   const updateMenuCount = () => {
     const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
     $('.menu-count').innerText = `총 ${menuCount}개`;
-  } 
+  };
 
   const checkMenuName = (name) => {
     if (name === '') {
       alert('값을 입력해주세요.');
       return;
-    }  
-  }
+    }
+  };
 
   const addMenuName = () => {
     const espressoMenuName = $('#espresso-menu-name').value;
@@ -50,18 +45,26 @@ function App() {
   };
 
   const updatedMenuName = (e) => {
-    const $menuName = e.target.closest('li').querySelector('.menu-name') 
-    const updatedMenuName = prompt('메뉴 이름을 수정해주세요.', $menuName.innerText)
+    const $menuName = e.target.closest('li').querySelector('.menu-name');
+    const updatedMenuName = prompt(
+      '메뉴 이름을 수정해주세요.',
+      $menuName.innerText
+    );
     checkMenuName(updatedMenuName);
     $menuName.innerText = updatedMenuName;
-  }
+  };
 
   const removeMenuName = (e) => {
     if (confirm('정말 메뉴를 삭제하시겠습니까?')) {
       e.target.closest('li').remove();
       updateMenuCount();
     }
-  }
+  };
+
+  // Form 태그가 자동으로 전송되는 것을 막아줌
+  $('#espresso-menu-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+  });
 
   // 메뉴의 입력 받기
   $('#espresso-menu-submit-button').addEventListener('click', () => {
@@ -81,18 +84,17 @@ function App() {
 
   // 메뉴 수정
   $('#espresso-menu-list').addEventListener('click', (e) => {
-    if (e.target.classList.contains("menu-edit-button")) {
+    if (e.target.classList.contains('menu-edit-button')) {
       updatedMenuName(e);
     }
   });
 
   // 메뉴 삭제
   $('#espresso-menu-list').addEventListener('click', (e) => {
-    if (e.target.classList.contains("menu-remove-button")) {
+    if (e.target.classList.contains('menu-remove-button')) {
       removeMenuName(e);
     }
   });
-
 }
 
 App();
